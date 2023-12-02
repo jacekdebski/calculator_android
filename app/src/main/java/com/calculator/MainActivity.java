b81f55e17631386f1c821c0e908df3799acf18c2
Jacek Debski <jacek.debski@hotmail.com>
2023-12-02 14:41:52 +0100

    add exit button

--- app/src/main/java/com/calculator/MainActivity.java
+++ app/src/main/java/com/calculator/MainActivity.java
@@ -3,6 +3,9 @@ package com.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button exitButton = findViewById(R.id.exitButton);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.i("Main Activity", "ExitButton clicked. Calculator will close.");
+                finish();
+            }
+        });
     }
 }
