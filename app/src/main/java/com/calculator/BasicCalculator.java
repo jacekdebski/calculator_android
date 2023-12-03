8ec7a4c4d01347a81509c71cd46a519d57233664
Jacek Debski <jacek.debski@hotmail.com>
2023-12-03 17:11:11 +0100

    displaying text when user press button

---
+++ app/src/main/java/com/calculator/BasicCalculator.java
@@ -0,0 +1,10 @@
+package com.calculator;
+
+public class BasicCalculator {
+    private Double firstNumber;
+
+    public Double onClickOne(Double number){
+        this.firstNumber = number;
+        return number;
+    }
+}
--- app/src/main/java/com/calculator/BasicCalculatorActivity.java
+++ app/src/main/java/com/calculator/BasicCalculatorActivity.java
@@ -3,6 +3,9 @@ package com.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class BasicCalculatorActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class BasicCalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_calculator);
+
+        Button oneButton = findViewById(R.id.oneButton);
+        oneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BasicCalculator basicCalculator = new BasicCalculator();
+                Double result = basicCalculator.onClickOne(1.0);
+                setPromptText(result.toString());
+            }
+        });
+    }
+
+    void setPromptText(String textToWrite) {
+        TextView promptTextView = findViewById(R.id.promptTextView);
+        promptTextView.setText(textToWrite);
     }
 }
