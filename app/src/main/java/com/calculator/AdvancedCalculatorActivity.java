2d90537582b3a2b72b689325fa2f58f53d7bd534
Jacek Debski <jacek.debski@hotmail.com>
2023-12-08 20:36:19 +0100

    rename id for custom exponentiation button

--- app/src/main/java/com/calculator/AdvancedCalculatorActivity.java
+++ app/src/main/java/com/calculator/AdvancedCalculatorActivity.java
@@ -1,12 +1,9 @@
 package com.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class AdvancedCalculatorActivity extends BasicCalculatorActivity {
 
@@ -22,7 +19,7 @@ public class AdvancedCalculatorActivity extends BasicCalculatorActivity {
     }
 
     private void initAdvancedButtonsListeners() {
-        Button customExponentiationButton = findViewById(R.id.customExponentiation);
+        Button customExponentiationButton = findViewById(R.id.customExponentiationButton);
         customExponentiationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
