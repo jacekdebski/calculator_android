aac00834f1986e2cca8e113f37d77e6e58b22b08
Jacek Debski <jacek.debski@hotmail.com>
2024-02-03 05:05:53 +0100

    init landscape mode with two fragments

--- app/src/main/java/com/calculator/WeatherInfoActivity.java
+++ app/src/main/java/com/calculator/WeatherInfoActivity.java
@@ -19,42 +19,80 @@ public class WeatherInfoActivity extends AppCompatActivity {
 
         WeatherInfoManager.init(getApplicationContext());
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .setReorderingAllowed(true)
-                    .add(R.id.weather_fragment, BasicWeatherInfoFragment.class, null)
-                    .commit();
-
+        if (getResources().getConfiguration().orientation == android.content.res.Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_weather_info);
+        } else {
+            setContentView(R.layout.activity_weather_info);
         }
-        setContentView(R.layout.activity_weather_info);
 
-        Button basicWeatherInfoButton = findViewById(R.id.basicWeatherInfoButton);
-        basicWeatherInfoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Fragment basicWeatherInfoFragment = new BasicWeatherInfoFragment();
-                setFragment(basicWeatherInfoFragment);
-
+        if (savedInstanceState == null) {
+            if (getResources().getConfiguration().orientation == android.content.res.Configuration.ORIENTATION_LANDSCAPE) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.weather_basic_fragment, new BasicWeatherInfoFragment());
+                fragmentTransaction.add(R.id.weather_additional_fragment, new AdditionalWeatherInfoFragment());
+                fragmentTransaction.commit();
+            } else {
+                getSupportFragmentManager().beginTransaction()
+                        .setReorderingAllowed(true)
+                        .add(R.id.weather_fragment, BasicWeatherInfoFragment.class, null)
+                        .commit();
             }
-        });
+        }
 
-        Button additionalWeatherInfoButton = findViewById(R.id.additionalWeatherInfoButton);
-        additionalWeatherInfoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AdditionalWeatherInfoFragment additionalWeatherInfoFragment = new AdditionalWeatherInfoFragment();
-                setFragment(additionalWeatherInfoFragment);
-            }
-        });
+        if (getResources().getConfiguration().orientation == android.content.res.Configuration.ORIENTATION_LANDSCAPE) {
 
-        Button settingsButton = findViewById(R.id.settingsButton);
-        settingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SettingsFragment settingsFragment = new SettingsFragment();
-                setFragment(settingsFragment);
-            }
-        });
+            Button weatherInfoButton = findViewById(R.id.weatherInfoButton);
+            weatherInfoButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    BasicWeatherInfoFragment basicWeatherInfoFragment = new BasicWeatherInfoFragment();
+                    setFragment(basicWeatherInfoFragment, R.id.weather_basic_fragment);
+
+                    AdditionalWeatherInfoFragment additionalWeatherInfoFragment = new AdditionalWeatherInfoFragment();
+                    setFragment(additionalWeatherInfoFragment, R.id.weather_additional_fragment);
+                }
+            });
+
+            Button settingsButton = findViewById(R.id.settingsLandscapeButton);
+            settingsButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    SettingsFragment settingsFragment = new SettingsFragment();
+                    setFragment(settingsFragment, R.id.weather_additional_fragment);
+                }
+            });
+
+        } else {
+            Button basicWeatherInfoButton = findViewById(R.id.basicWeatherInfoButton);
+            basicWeatherInfoButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Fragment basicWeatherInfoFragment = new BasicWeatherInfoFragment();
+                    setFragment(basicWeatherInfoFragment, R.id.weather_fragment);
+
+                }
+            });
+
+            Button additionalWeatherInfoButton = findViewById(R.id.additionalWeatherInfoButton);
+            additionalWeatherInfoButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AdditionalWeatherInfoFragment additionalWeatherInfoFragment = new AdditionalWeatherInfoFragment();
+                    setFragment(additionalWeatherInfoFragment, R.id.weather_fragment);
+                }
+            });
+
+            Button settingsButton = findViewById(R.id.settingsButton);
+            settingsButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    SettingsFragment settingsFragment = new SettingsFragment();
+                    setFragment(settingsFragment, R.id.weather_fragment);
+                }
+            });
+        }
+
 
         WeatherInfoManager.setWeatherInfoLoadListener(new WeatherInfoLoadListener() {
             @Override
@@ -65,10 +103,10 @@ public class WeatherInfoActivity extends AppCompatActivity {
         });
     }
 
-    private void setFragment(Fragment fragment) {
+    private void setFragment(Fragment fragment, int containerViewId) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.weather_fragment, fragment);
+        fragmentTransaction.replace(containerViewId, fragment);
         fragmentTransaction.commit();
     }
 
